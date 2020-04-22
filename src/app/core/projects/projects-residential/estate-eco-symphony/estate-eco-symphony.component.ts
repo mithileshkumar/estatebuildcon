7829e11c95c4fd69aaf356d15981edc20828fc86
Mithilesh Kumar <mithileshkumar0108@gmail.com>
2020-04-22 15:01:44 +0530

    added 3d floor plans

--- src/app/core/projects/projects-residential/estate-eco-symphony/estate-eco-symphony.component.ts
+++ src/app/core/projects/projects-residential/estate-eco-symphony/estate-eco-symphony.component.ts
@@ -9,12 +9,19 @@ import { MailService } from 'src/app/services/mail.service';
 export class EstateEcoSymphonyComponent implements OnInit {
   mailMessage = '';
   floorPlans = [
-    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/1.jpg', details: 'Site Plan & Drainage Plan' },
-    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/2.jpg', details: 'Basement Floor Plan Parking' },
-    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/3.jpg', details: 'Ground Floor Plan' },
-    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/4.jpg', details: 'First Floor Plan' },
-    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/5.jpg', details: 'Second To Fourth Floor Plan' },
-    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/6.jpg', details: '5th & 6th Floor Plan' }
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/1.jpg', details: '4BHK 3150 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/2.jpg', details: '3BHK 1428 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/3.jpg', details: '3BHK 1557 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/4.jpg', details: '3BHK 1675 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/5.jpg', details: '4BHK 3458 Sq. Ft.' }
+  ];
+
+  floorPlans3D = [
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/3d/sf1428.jpg', details: '1428 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/3d/sf1557.jpg', details: '1557 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/3d/sf1675.jpg', details: '1675 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/3d/sf3190.jpg', details: '3190 Sq. Ft.' },
+    { img: '/assets/compressed-floor-plans/residential/estate-eco-symphony/3d/sf3458.jpg', details: '3458 Sq. Ft.' }
   ];
 
   gallery = [
