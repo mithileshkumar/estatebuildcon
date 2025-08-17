ec8312841eaee04eb431305d154754a901119f82
Mithilesh Kumar <mithileshkumar0108@gmail.com>
2025-08-17 15:03:23 +0530

    added route change for sub paths in SPA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-HR0CJ5FKSL', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
